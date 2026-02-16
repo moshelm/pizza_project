8657c24d585230ec2442d24da4f595a3f6a3cbc0
Moshe <moshe@example.com>
2026-02-16 14:40:38 +0200

    fix: clean code and one iteration for actions

--- api/app/routes.py
+++ api/app/routes.py
@@ -3,8 +3,7 @@ import json
 import db.connection as mongo
 from schemas import RequestsFile
 from pydantic import ValidationError
-from confluent_kafka import Producer
-from producer import insert_to_kafka
+from producer import insert_to_kafka, flush
 
 router = APIRouter()
 
@@ -13,19 +12,22 @@ async def upload_json_file(file: UploadFile = File(...)):
     try: 
         content = await file.read()
         data = json.loads(content)
-        valid_data = [RequestsFile(**item).model_dump() | {"status":"PREPARING"} for item in data]
-
+        for item in data:
+            valid_item = RequestsFile(**item).model_dump()
+            valid_item['status'] ="PREPARING"
+            mongo.collection.insert_one(valid_item)
+            insert_to_kafka(valid_item) 
+        flush()      
+        
+        return {"massage":"success"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=f"mongo failed {str(e)}")
+    
     except ValidationError as e:
         raise HTTPException(status_code=400,detail=f"not valid file {e.errors()}")
 
     except json.JSONDecodeError as e:
         raise HTTPException(status_code=400, detail=f'error reading file {str(e)}')
-    try: 
-        for msg in valid_data:
-            mongo.collection.insert_one(msg)
-            insert_to_kafka(msg)       
-        return {"massage":"success"}
-    except Exception as e:
-        raise HTTPException(status_code=400,detail=f"mongo failed {str(e)}")
     
     
