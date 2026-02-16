dc8ea5cde431d3e99ede26cdd8a035cf86fbad43
Moshe <moshe@example.com>
2026-02-16 12:39:15 +0200

    add: error handling and  validation pydantic

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter,UploadFile,File, HTTPException 
 import json
 import db.connection as mongo
+from schemas import RequestsFile
+from pydantic import ValidationError
+
 
 router = APIRouter()
 
@@ -9,10 +12,16 @@ async def upload_json_file(file: UploadFile = File(...)):
     try: 
         content = await file.read()
         data = json.loads(content)
+        valid_data = [RequestsFile(**item).model_dump() | {"status":"PREPARING"} for item in data]
+
+    except ValidationError as e:
+        raise HTTPException(status_code=400,detail=f"not valid file {e.errors()}")
+
     except json.JSONDecodeError as e:
         raise HTTPException(status_code=400, detail=f'error reading file {str(e)}')
-    try:
-        mongo.collection.insert_many(data)
+    try: 
+        mongo.collection.insert_many(valid_data)
+        
         return {"massage":"success"}
     except Exception as e:
         raise HTTPException(status_code=400,detail=f"mongo failed {str(e)}")
