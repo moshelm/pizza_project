3c271f57d423e4b82f5bdd6fc2bdbf5bb7b48a0d
Moshe <moshe@example.com>
2026-02-16 12:56:15 +0200

    create kafka producer

---
+++ api/app/producer.py
@@ -0,0 +1,20 @@
+from confluent_kafka import Producer, Message
+import os 
+import json
+
+KAFKA = os.getenv("KAFKA",'localhost:9092')
+CONFIG = {'bootstrap.servers': KAFKA}
+
+producer = Producer(CONFIG)
+
+def delivery_report(err: Message, msg: Message):
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()}, {msg.partition()}')
+
+def insert_to_kafka(data:dict):
+    producer.produce(topic='pizza-orders',key=data.get("order_id"), value=json.dumps(data.encode('utf-8')), callback=delivery_report)
+    producer.poll(0)
+
+producer.flush()
