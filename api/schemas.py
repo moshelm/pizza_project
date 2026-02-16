d2636e6419aa66abe25b428ea715988ef72c6201
Moshe <moshe@example.com>
2026-02-16 12:38:18 +0200

    fix: change to optional

--- api/schemas.py
+++ api/schemas.py
@@ -2,8 +2,14 @@ from pydantic import BaseModel
 
 class RequestsFile(BaseModel):
     order_id: str
-    pizza_type: str
+    pizza_type: str 
     size: str
     quantity: int
     is_delivery: bool
-    special_instructions:  str
+    special_instructions : Optional[str]
+
+# class ResponseMongo(str, Enum):
+#      PREPARING =  "preparing"
+
+# class insert_mongo(RequestsFile):
+#      status : ResponseMongo
