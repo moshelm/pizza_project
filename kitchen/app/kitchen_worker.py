9c35ed569595264cf917a225c904740983ef8f01
Moshe <moshe@example.com>
2026-02-17 11:49:10 +0200

    fix: delete delay 15 sec in poll function

--- kitchen/app/kitchen_worker.py
+++ kitchen/app/kitchen_worker.py
@@ -16,7 +16,7 @@ consumer = Consumer(CONFIG_CONSUMER)
 def subscribe():
     consumer.subscribe(['pizza-orders'])
     while True:
-        msg = consumer.poll(15)
+        msg = consumer.poll()
         if msg is None:
             continue
         if msg.error():
