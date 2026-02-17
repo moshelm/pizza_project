e3e06ba961ebb4c528fcb29dcbe732f5d69357af
Moshe <moshe@example.com>
2026-02-17 16:45:15 +0200

    feat: new logic

--- text/app/text_worker.py
+++ text/app/text_worker.py
@@ -4,6 +4,8 @@ import connection_mongo as mongo
 import json 
 import re
 from bson import ObjectId
+from preprocessor import *
+
 
 KAFKA = os.getenv("KAFKA","localhost:9092")
 
@@ -16,21 +18,47 @@ consumer = Consumer(CONFIG)
 
 def subscribe():
     consumer.subscribe(['pizza-orders'])
-    while True:
-        msg = consumer.poll()
-        if msg is None:
-            continue
-        if msg.error():
-            print(f'error in msg {msg.error()}')
-            continue
-        key = msg.key().decode("utf-8")
-        data = json.loads(msg.value().decode("utf-8"))
-        dangers = ["allergy", "peanut", "gluten"]
-        for dng in dangers:
-            if dng in data["special_instructions"]:
-                data['allergies_flaged'] = True
-        clean_text = re.sub(r'[^a-zA-Z ]','',data["special_instructions"].upper())
-        clean_text = re.sub(r'\s+', ' ', clean_text).strip()
-        data["cleaned_protocol"] =  clean_text    
-        data['_id']= ObjectId(data['_id'])
-        mongo.collection.replace_one({'order_id':key},data)
+    try:
+        while True:
+            msg = consumer.poll()
+
+            # error handling
+            if msg is None:
+                continue
+            if msg.error():
+                print(f'error in msg {msg.error()}')
+                continue
+
+            # getting data from kafka
+            key = msg.key().decode("utf-8")
+            data = json.loads(msg.value().decode("utf-8"))
+            
+            # clean and logic data 
+            logic_text_worker(data)
+            data_info = get_info_by_pizza_type(data['pizza_type'])
+            data_info = clean_data(data_info)
+            data['information'] = data_info
+            insert_to_kafka(data)
+            
+    
+    finally:
+        flush()
+
+
+
+def logic_text_worker(data:dict):
+    is_danger(data)
+    data["cleaned_protocol"] =  clean_data(data["special_instructions"])    
+    data['_id']= ObjectId(data['_id'])
+
+
+def is_danger(data:dict):
+    dangers = ["allergy", "peanut", "gluten"]
+    for dng in dangers:
+        if dng in data["special_instructions"]:
+            data['allergies_flaged'] = True
+
+def clean_data(data : str) -> str:
+    clean_text = re.sub(r'[^a-zA-Z ]','',data.upper())
+    clean_text = re.sub(r'\s+', ' ', clean_text).strip()
+    return clean_text
