6aa9363405ef0a456322f4ae5576f7b49fc4090e
Moshe <moshe@example.com>
2026-02-17 11:47:19 +0200

    fix: env variables to correct names of variable

--- text/app/connection_mongo.py
+++ text/app/connection_mongo.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 import os 
 
 MONGO_HOST = os.getenv('MONGO_HOST','localhost')
-MONGO_PORT = os.getenv('MONGO_HOST','27017')
-MONGO_DATABASE = os.getenv('MONGO_HOST','db')
-MONGO_COLLECTION = os.getenv('MONGO_HOST','orders')
+MONGO_PORT = os.getenv('MONGO_PORT','27017')
+MONGO_DATABASE = os.getenv('MONGO_DATABASE','db')
+MONGO_COLLECTION = os.getenv('MONGO_COLLECTION','orders')
 
 CONFIG = f"mongodb://{MONGO_HOST}:{MONGO_PORT}"
 
